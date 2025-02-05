34ad37a51e4cbec3b994669cb625f1712061b5ab
janeburugu <janeburugu69@gmail.com>
2025-02-05 18:18:22 +0300

    docker containerization

--- src/main/java/com/example/communication/ussd/web/controllers/UssdController.java
+++ src/main/java/com/example/communication/ussd/web/controllers/UssdController.java
@@ -7,6 +7,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
 @RestController
 @RequestMapping("ussd")
 public class UssdController {
--- src/main/java/com/example/communication/whatsapp/web/controllers/WhatsAppController.java
+++ src/main/java/com/example/communication/whatsapp/web/controllers/WhatsAppController.java
@@ -6,6 +6,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 @RestController
 @RequestMapping("whatsapp")
 public class WhatsAppController {
